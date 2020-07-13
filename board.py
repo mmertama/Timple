3bd04e95a55c2fa8176143cbb3204de0c41af367
Markus Mertama <markus.mertama@intopalo.com>
2020-07-14 01:14:22 +0300

    Initial release

---
+++ board.py
@@ -0,0 +1,159 @@
+import os
+import sys
+import math
+import json
+import Telex
+
+FEATHER = 10
+
+
+class Peg:
+    def __init__(self, color, slot):
+        self.color = color
+        self.slot = slot
+
+    def draw(self, frame):
+        frame.begin_path()
+        frame.arc(self.slot.x - self.slot.size / 2, self.slot.y - self.slot.size / 2, self.slot.size, 0, 2 * math.pi)
+        frame.fill_style(self.color)
+        frame.fill()
+
+
+class Slot:
+    def __init__(self, d):
+        self.x = d['x']
+        self.y = d['y']
+        self.size = float(d['size'])
+        self.selected = False
+        self.peg = Peg(d['color'], self) if d['color'] else None
+
+    def draw(self, frame):
+        frame.begin_path()
+        frame.arc(self.x - self.size / 2, self.y - self.size / 2, self.size, 0, 2 * math.pi)
+        frame.stroke()
+
+        if self.selected:
+            frame.begin_path()
+            frame.arc(self.x - self.size / 2, self.y - self.size / 2, self.size * 2, 0, 2 * math.pi)
+            frame.stroke_style("#2F2F2F")
+            frame.stroke()
+
+        if self.peg:
+            self.peg.draw(frame)
+
+    def clicked(self, x, y):
+        return self.is_in(x, y)
+
+    def is_in(self, x, y):
+        return math.fabs(self.x - x) <= (self.size + FEATHER) \
+               and math.fabs(self.y - y) <= (self.size + FEATHER)
+
+    def select(self, select):
+        self.selected = select
+
+
+class Ring:
+    def __init__(self, d):
+        self.x = d['x']
+        self.y = d['y']
+        self.slots = [Slot(s) for s in d['slots']]
+
+    def draw(self, frame):
+        for s in self.slots:
+            s.draw(frame)
+
+    def clicked(self, x, y):
+        for s in self.slots:
+            if s.clicked(x, y):
+                return s
+        return None
+
+
+class Home:
+    def __init__(self, d):
+        self.slots = [Slot(s) for s in d['slots']]
+
+    def draw(self, frame):
+        for s in self.slots:
+            s.draw(frame)
+
+    def clicked(self, x, y):
+        for s in self.slots:
+            if s.clicked(x, y):
+                return s
+        return None
+
+
+class Game:
+    def __init__(self, data):
+        self.width = data['width']
+        self.height = data['height']
+        self.ring = Ring(data['ring'])
+        self.starts = [Home(s) for s in data['starts']]
+        self.goals = [Home(s) for s in data['goals']]
+        self.selected = None
+
+    def draw(self, frame_composer):
+        self.ring.draw(frame_composer)
+        for s in self.starts:
+            s.draw(frame_composer)
+        for g in self.goals:
+            g.draw(frame_composer)
+
+    def clicked(self, x, y):
+        if self.swap(self.ring.clicked(x, y)):
+            return True
+        for s in self.starts:
+            if self.swap(s.clicked(x, y)):
+                return True
+        for g in self.goals:
+            if self.swap(g.clicked(x, y)):
+                return True
+        return False
+
+    def swap(self, selected):
+        if not selected:
+            return False
+        if self.selected:
+            self.selected.select(False)
+        self.selected = selected
+        selected.select(True)
+        return True
+
+
+def main():
+    # Telex.set_debug()
+    print(Telex.version())
+    ui_file = os.path.dirname(os.path.realpath(__file__)) + '/gui/timple.html'
+
+    ui = Telex.Ui(ui_file)
+
+    canvas = Telex.CanvasElement(ui, "canvas")
+
+    with open("data.json", 'r') as f:
+        data = json.load(f)
+
+    game = Game(data)
+
+    frame_composer = Telex.FrameComposer()
+
+    game.draw(frame_composer)
+
+    canvas.draw_frame(frame_composer)
+
+    ui.on_error(lambda e: sys.exit(e))
+
+    def on_click(event):
+        fc = Telex.FrameComposer()
+        fc.clear_rect(Telex.Rect(0, 0, game.width, game.height))
+        game.clicked(float(event.properties['clientX']), float(event.properties['clientY']))
+        game.draw(fc)
+        canvas.draw_frame(fc)
+
+    canvas.subscribe('click', on_click, ["clientX", "clientY"])
+
+    ui.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ makeboard.py
@@ -0,0 +1,109 @@
+import math
+import json
+
+
+class Slot:
+    def __init__(self, x, y, size, color=None):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+
+    def data(self):
+        return {'x': self.x,
+                'y': self.y,
+                'size': self.size,
+                'color': self.color}
+
+
+class Ring:
+    def __init__(self, x, y, rad, size, count):
+        self.x = x
+        self.y = y
+        self.slots = []
+        step = (2 * math.pi) / count
+        r = 0
+        for i in range(0, count):
+            xx = math.sin(r) * rad
+            yy = math.cos(r) * rad
+            self.slots.append(Slot(xx + x, yy + y, size))
+            r += step
+
+    def data(self):
+        return {'x': self.x,
+                'y': self.y,
+                'slots': [s.data() for s in self.slots]
+                }
+
+
+class Home:
+    def __init__(self):
+        self.slots =[]
+
+    def data(self):
+        return {'slots': [s.data() for s in self.slots]}
+
+
+class Start(Home):
+    def __init__(self, x, y, angle, distance, width, size, count, direction, color):
+        self.slots = []
+        px = math.sin(angle) * distance
+        py = math.cos(angle) * distance
+
+        dx = math.sin(angle + direction) * width / count
+        dy = math.cos(angle + direction) * width / count
+
+        x0 = (x + px) - (dx * ((count - 1) / 2))
+        y0 = (y + py) - (dy * ((count - 1) / 2))
+
+        for i in range(0, count):
+            self.slots.append(Slot(x0, y0, size, color))
+            x0 += dx
+            y0 += dy
+
+
+class Goal(Home):
+    def __init__(self, x, y, angle, distance, width, size, count, direction):
+        self.slots = []
+        px = math.sin(angle) * distance
+        py = math.cos(angle) * distance
+
+        dx = math.sin(angle + direction) * width / count
+        dy = math.cos(angle + direction) * width / count
+
+        x0 = (x + px)
+        y0 = (y + py)
+
+        for i in range(0, count):
+            self.slots.append(Slot(x0, y0, size))
+            x0 += dx
+            y0 += dy
+
+
+def main():
+    encoder = json.JSONEncoder(indent=1)
+    ball_rad = 6
+    ring = Ring(250, 250, 150, ball_rad, 20)
+    starts = []
+
+    colors = ["#FF0000", "#00FF00", "#0000FF", "#00FFFF"]
+
+    for a in range(0, 4):
+        starts.append(Start(250, 250, a * (math.pi / 2), 200, 100, ball_rad, 4, math.pi / 2, colors[a]))
+
+    goals = []
+    for a in range(0, 4):
+        goals.append(Goal(250, 250, a * (math.pi / 2), 20, 100, ball_rad, 4, 0))
+
+    data = encoder.encode({'width': 500,
+                           'height': 500,
+                           'ring': ring.data(),
+                           'starts': [s.data() for s in starts],
+                           'goals': [g.data() for g in goals]})
+
+    with open("data.json", 'w') as f:
+        f.write(data)
+
+
+if __name__ == "__main__":
+    main()
